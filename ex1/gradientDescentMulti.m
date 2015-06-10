4edf19f747fcb64e117a93a97185b1afe2bab679
Wojtek Snarski <wsnarski@carthage.edu>
2015-06-09 22:38:24 -0500

    Gradient Descent multivariant complete

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -11,27 +11,23 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+    tSize = rows(theta);
+    tempTheta = theta;
+    for tIter = 1:tSize
+    %  singleTheta = theta(thetaiter);
+      tempTheta(tIter) = tempTheta(tIter) - (alpha*1/m * sum(((X * theta) - y) .* X(:, tIter)));
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
-
+    theta = tempTheta;
 end
