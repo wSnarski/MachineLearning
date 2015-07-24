0ce3bfe40b4c9a7eea6f2eb337b15751ce3cbe9f
Wojtek Snarski <wsnarski@carthage.edu>
2015-07-23 20:02:52 -0500

    Cost function and Gradient completed

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,13 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+J = (1/m) * ((-y' * log(sigmoid(X * theta))) - ((1 - y)' * log(1 - sigmoid(X * theta))));
 
 
-
-
-
-
-
+%This works without iterating because of matrix * matrix 
+grad = (1/m) * ((sigmoid(X * theta) - y)' * X)';
 
 % =============================================================
 
